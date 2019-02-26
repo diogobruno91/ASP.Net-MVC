83871873b64f5dc79222f6adbdde14331aa8b869
Diogo Bruno <diogobruno91@gmail.com>
2019-02-25 21:20:05 -0300

    aula01

---
+++ Aula01/Aula01/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Aula01.Startup))]
+namespace Aula01
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
