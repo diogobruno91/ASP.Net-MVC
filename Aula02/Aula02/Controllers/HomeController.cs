f8c2a4f1458e42d0ffeb94356eee85ab1ff9b1f2
dbruno <diogobruno91@gmail.com>
2019-02-26 14:04:18 -0300

    Aula02

---
+++ Aula02/Aula02/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Aula02.Models;
+
+namespace Aula02.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var pessoa = new Pessoa
+                            {
+                                PessoaId = 1,
+                                Nome = "Diogo Bruno",
+                                Twitter = "@dbruno"
+                            };
+            return View(pessoa);
+        }
+        [HttpPost]
+        public ActionResult Lista(Pessoa pessoa)
+        {
+            ViewData["PessoaId"] = pessoa.PessoaId; 
+            ViewData["Nome"] = pessoa.Nome; 
+            ViewData["Twitter"] = pessoa.Twitter;
+
+            return View(pessoa);
+        }
+    }
+}
---
+++ Aula02/Aula02/Models/Pessoa.cs
@@ -0,0 +1,9 @@
+﻿namespace Aula02.Models
+{
+    public class Pessoa
+    {
+        public int PessoaId { get; set; }
+        public string Nome { get; set; }
+        public string Twitter { get; set; }
+    }
+}
